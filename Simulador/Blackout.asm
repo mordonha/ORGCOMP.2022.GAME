17dc64e0b52a993fcd7a51c837f91290316747f7
minoru <>
2022-01-06 20:47:06 -0300

    Created gitignore for Sublime packages & Added game's skeleton

---
+++ Simulador/Blackout.asm
@@ -0,0 +1,17 @@
+;Blackout
+
+;Jansen Caik Ferreira Freitas
+;Leonardo Minoru Iwashima
+;Paulo Marcos Ordonha
+
+;Variaveis e constantes
+
+;______________________
+
+;Main
+
+;____
+
+;Funções
+
+;_______
