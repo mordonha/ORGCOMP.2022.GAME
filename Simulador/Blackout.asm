3db4befdf4ece7da37dfd41b72996028ec20dd0e
minoru <>
2022-01-06 21:15:37 -0300

    Now, its possible to draw scenarios

--- Simulador/Blackout.asm
+++ Simulador/Blackout.asm
@@ -1,17 +1,310 @@
-;Blackout
+; Blackout
 
-;Jansen Caik Ferreira Freitas
-;Leonardo Minoru Iwashima
-;Paulo Marcos Ordonha
+; Jansen Caik Ferreira Freitas
+; Leonardo Minoru Iwashima
+; Paulo Marcos Ordonha
 
-;Variaveis e constantes
+; Variaveis e constantes
 
-;______________________
+; ______________________
 
-;Main
+; main
+main:
+call ApagaTela
+	loadn R1, #tela1Linha0	; Endereco onde comeca a primeira linha do cenario!!
+	loadn R2, #1536  			; cor branca!
+	call ImprimeTela2   		;  Rotina de Impresao de Cenario na Tela Inteira
+    
+	loadn R1, #tela2Linha0	; Endereco onde comeca a primeira linha do cenario!!
+	loadn R2, #512  			; cor branca!
+	call ImprimeTela2   		;  Rotina de Impresao de Cenario na Tela Inteira
+    
+	loadn R1, #tela3Linha0	; Endereco onde comeca a primeira linha do cenario!!
+	loadn R2, #2816   			; cor branca!
+	call ImprimeTela2   		;  Rotina de Impresao de Cenario na Tela Inteira
 
-;____
+	loadn R1, #tela4Linha0	; Endereco onde comeca a primeira linha do cenario!!
+	loadn R2, #256   			; cor branca!
+	call ImprimeTela2   		;  Rotina de Impresao de Cenario na Tela Inteira
+; /main
 
-;Funções
 
-;_______
+
+
+
+
+;************************************************************
+;                         FUNÇÕES
+;************************************************************
+
+	
+; IMPRIME TELA2_______________________________________
+ImprimeTela2: 	;  Rotina de Impresao de Cenario na Tela Inteira
+		;  r1 = endereco onde comeca a primeira linha do Cenario
+		;  r2 = cor do Cenario para ser impresso
+
+	push r0	; protege o r3 na pilha para ser usado na subrotina
+	push r1	; protege o r1 na pilha para preservar seu valor
+	push r2	; protege o r1 na pilha para preservar seu valor
+	push r3	; protege o r3 na pilha para ser usado na subrotina
+	push r4	; protege o r4 na pilha para ser usado na subrotina
+	push r5	; protege o r5 na pilha para ser usado na subrotina
+	push r6	; protege o r6 na pilha para ser usado na subrotina
+
+	loadn R0, #0  	; posicao inicial tem que ser o comeco da tela!
+	loadn R3, #40  	; Incremento da posicao da tela!
+	loadn R4, #41  	; incremento do ponteiro das linhas da tela
+	loadn R5, #1200 ; Limite da tela!
+	loadn R6, #tela0Linha0	; Endereco onde comeca a primeira linha do cenario!!
+	
+   ImprimeTela2_Loop:
+		call ImprimeStr2
+		add r0, r0, r3  	; incrementaposicao para a segunda linha na tela -->  r0 = R0 + 40
+		add r1, r1, r4  	; incrementa o ponteiro para o comeco da proxima linha na memoria (40 + 1 porcausa do /0 !!) --> r1 = r1 + 41
+		add r6, r6, r4  	; incrementa o ponteiro para o comeco da proxima linha na memoria (40 + 1 porcausa do /0 !!) --> r1 = r1 + 41
+		cmp r0, r5			; Compara r0 com 1200
+		jne ImprimeTela2_Loop	; Enquanto r0 < 1200
+
+	pop r6	; Resgata os valores dos registradores utilizados na Subrotina da Pilha
+	pop r5
+	pop r4
+	pop r3
+	pop r2
+	pop r1
+	pop r0
+	rts		
+; /IMPRIME TELA2_________________________________________		
+
+	
+; IMPRIME STRING2________________________________________
+ImprimeStr2:	;  Rotina de Impresao de Mensagens:    r0 = Posicao da tela que o primeiro caractere da mensagem sera' impresso;  r1 = endereco onde comeca a mensagem; r2 = cor da mensagem.   Obs: a mensagem sera' impressa ate' encontrar "/0"
+	push r0	; protege o r0 na pilha para preservar seu valor
+	push r1	; protege o r1 na pilha para preservar seu valor
+	push r2	; protege o r1 na pilha para preservar seu valor
+	push r3	; protege o r3 na pilha para ser usado na subrotina
+	push r4	; protege o r4 na pilha para ser usado na subrotina
+	push r5	; protege o r5 na pilha para ser usado na subrotina
+	push r6	; protege o r6 na pilha para ser usado na subrotina
+	
+	
+	loadn r3, #'\0'	; Criterio de parada
+	loadn r5, #' '	; Espaco em Branco
+
+   ImprimeStr2_Loop:	
+		loadi r4, r1
+		cmp r4, r3		; If (Char == \0)  vai Embora
+		jeq ImprimeStr2_Sai
+		cmp r4, r5		; If (Char == ' ')  vai Pula outchar do espaco para na apagar outros caracteres
+		jeq ImprimeStr2_Skip
+		add r4, r2, r4	; Soma a Cor
+		outchar r4, r0	; Imprime o caractere na tela
+   		storei r6, r4
+   ImprimeStr2_Skip:
+		inc r0			; Incrementa a posicao na tela
+		inc r1			; Incrementa o ponteiro da String
+		inc r6
+		jmp ImprimeStr2_Loop
+	
+   ImprimeStr2_Sai:	
+	pop r6	; Resgata os valores dos registradores utilizados na Subrotina da Pilha
+	pop r5
+	pop r4
+	pop r3
+	pop r2
+	pop r1
+	pop r0
+	rts
+; /IMPRIME STRING2__________________________________________		
+
+; APAGA TELA________________________________________________
+ApagaTela:
+	push r0
+	push r1
+	
+	loadn r0, #1200		; apaga as 1200 posicoes da Tela
+	loadn r1, #' '		; com "espaco"
+	
+	   ApagaTela_Loop:	;;label for(r0=1200;r3>0;r3--)
+		dec r0
+		outchar r1, r0
+		jnz ApagaTela_Loop
+ 
+	pop r1
+	pop r0
+	rts	
+; /APAGA TELA________________________________________________
+
+;************************************************************
+
+
+
+
+
+;************************************************************
+;                          TELAS
+;************************************************************
+	
+; Declara uma tela vazia para ser preenchida em tempo de execussao:
+tela0Linha0  : string "                                        "
+tela0Linha1  : string "                                        "
+tela0Linha2  : string "                                        "
+tela0Linha3  : string "                                        "
+tela0Linha4  : string "                                        "
+tela0Linha5  : string "                                        "
+tela0Linha6  : string "                                        "
+tela0Linha7  : string "                                        "
+tela0Linha8  : string "                                        "
+tela0Linha9  : string "                                        "
+tela0Linha10 : string "                                        "
+tela0Linha11 : string "                                        "
+tela0Linha12 : string "                                        "
+tela0Linha13 : string "                                        "
+tela0Linha14 : string "                                        "
+tela0Linha15 : string "                                        "
+tela0Linha16 : string "                                        "
+tela0Linha17 : string "                                        "
+tela0Linha18 : string "                                        "
+tela0Linha19 : string "                                        "
+tela0Linha20 : string "                                        "
+tela0Linha21 : string "                                        "
+tela0Linha22 : string "                                        "
+tela0Linha23 : string "                                        "
+tela0Linha24 : string "                                        "
+tela0Linha25 : string "                                        "
+tela0Linha26 : string "                                        "
+tela0Linha27 : string "                                        "
+tela0Linha28 : string "                                        "
+tela0Linha29 : string "                                        "	
+
+; Declara e preenche tela linha por linha (40 caracteres):
+tela1Linha0  : string "                                        "
+tela1Linha1  : string "                                        "
+tela1Linha2  : string "                                        "
+tela1Linha3  : string "                                        "
+tela1Linha4  : string "                                        "
+tela1Linha5  : string "                                        "
+tela1Linha6  : string "                                        "
+tela1Linha7  : string "                                        "
+tela1Linha8  : string "                        @@@@            "
+tela1Linha9  : string "                      @@@@@@@@@@        "
+tela1Linha10 : string "                       @@@@@@@@@@@@     "
+tela1Linha11 : string "                         @@@            "
+tela1Linha12 : string "                                        "
+tela1Linha13 : string "                                        "
+tela1Linha14 : string "                                        "
+tela1Linha15 : string "                                        "
+tela1Linha16 : string "                                        "
+tela1Linha17 : string "                                        "
+tela1Linha18 : string "                                        "
+tela1Linha19 : string "                                        "
+tela1Linha20 : string "                                        "
+tela1Linha21 : string "                                        "
+tela1Linha22 : string "                                        "
+tela1Linha23 : string "                                        "
+tela1Linha24 : string "                                        "
+tela1Linha25 : string "                                        "
+tela1Linha26 : string "                                        "
+tela1Linha27 : string "                                        "
+tela1Linha28 : string "                                        "
+tela1Linha29 : string "                                        "
+
+
+
+; Declara e preenche tela linha por linha (40 caracteres):
+tela2Linha0  : string "                                        "
+tela2Linha1  : string "                                        "
+tela2Linha2  : string "                                        "
+tela2Linha3  : string "                                        "
+tela2Linha4  : string "                                        "
+tela2Linha5  : string "                                        "
+tela2Linha6  : string "                                        "
+tela2Linha7  : string "                                        "
+tela2Linha8  : string "                                        "
+tela2Linha9  : string "                                        "
+tela2Linha10 : string "                                        "
+tela2Linha11 : string "                                        "
+tela2Linha12 : string "                                        "
+tela2Linha13 : string "                                        "
+tela2Linha14 : string "                                        "
+tela2Linha15 : string "                                        "
+tela2Linha16 : string "                                        "
+tela2Linha17 : string "                     :  :               "
+tela2Linha18 : string "          :  :     :      :             "
+tela2Linha19 : string "       :       : :          :           "
+tela2Linha20 : string "     :        :    :         :          "
+tela2Linha21 : string "            :         :                 "
+tela2Linha22 : string "          :             :               "
+tela2Linha23 : string "         :                :             "
+tela2Linha24 : string "         :                 :            "
+tela2Linha25 : string "          :               :             "
+tela2Linha26 : string "                                        "
+tela2Linha27 : string "                                        "
+tela2Linha28 : string "                                        "
+tela2Linha29 : string "                                        "
+
+
+; Declara e preenche tela linha por linha (40 caracteres):
+tela3Linha0  : string "                                        "
+tela3Linha1  : string "                                        "
+tela3Linha2  : string "                                        "
+tela3Linha3  : string "                                        "
+tela3Linha4  : string "    .  |  .                             "
+tela3Linha5  : string "      OOO                               "
+tela3Linha6  : string "   - OOOOO -                            "
+tela3Linha7  : string "      OOO                               "
+tela3Linha8  : string "    .  |  .                             "
+tela3Linha9  : string "                                        "
+tela3Linha10 : string "                                        "
+tela3Linha11 : string "                                        "
+tela3Linha12 : string "                                        "
+tela3Linha13 : string "                                        "
+tela3Linha14 : string "                                        "
+tela3Linha15 : string "                                        "
+tela3Linha16 : string "                                        "
+tela3Linha17 : string "                                        "
+tela3Linha18 : string "                                        "
+tela3Linha19 : string "                                        "
+tela3Linha20 : string "                **                      "
+tela3Linha21 : string "                                        "
+tela3Linha22 : string "                                        "
+tela3Linha23 : string "                                        "
+tela3Linha24 : string "                                        "
+tela3Linha25 : string "                                        "
+tela3Linha26 : string "                                        "
+tela3Linha27 : string "                                        "
+tela3Linha28 : string "                                        "
+tela3Linha29 : string "                                        "
+
+
+
+tela4Linha0  : string "                                        "
+tela4Linha1  : string "                                        "
+tela4Linha2  : string "                                        "
+tela4Linha3  : string "                                        "
+tela4Linha4  : string "                                        "
+tela4Linha5  : string "                                        "
+tela4Linha6  : string "                                        "
+tela4Linha7  : string "                                        "
+tela4Linha8  : string "                                        "
+tela4Linha9  : string "                                        "
+tela4Linha10 : string "                                        "
+tela4Linha11 : string "                                        "
+tela4Linha12 : string "                                        "
+tela4Linha13 : string "                                        "
+tela4Linha14 : string "                                        "
+tela4Linha15 : string "                                        "
+tela4Linha16 : string "                                        "
+tela4Linha17 : string "                                        "
+tela4Linha18 : string "                                        "
+tela4Linha19 : string "                                        "
+tela4Linha20 : string "                                        "
+tela4Linha21 : string "                 ..                     "
+tela4Linha22 : string "                  ..                    "
+tela4Linha23 : string "                   ..                   "
+tela4Linha24 : string "                   ..                   "
+tela4Linha25 : string "                  ...                   "
+tela4Linha26 : string "                 ...                    "
+tela4Linha27 : string "                ...                     "
+tela4Linha28 : string "               ....                     "
+tela4Linha29 : string "              .....                     "
+;Telas_fim
